4f533ef6133ebfaa9f23c03c216b889856ca66a7
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-10-19 17:00:11 +0700

    update

---
+++ dao nguoc phan tu/Stack.php
@@ -0,0 +1,22 @@
+<?php
+class Stack
+{
+    public array $stack;
+
+    public function __construct()
+    {
+        $this->stack = [];
+    }
+    public function push($value)
+    {
+//        if (count($this->stack)<$this->value){
+            array_unshift($this->stack,$value);
+//        }else{
+//            echo('stack is full!');
+//        }
+    }
+    public function pop()
+    {
+            return array_pop($this->stack);
+    }
+}
